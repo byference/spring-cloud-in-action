be3db33eff66ea951ceeccfbe3f83814f53f6085
baifenghe <1170126481@qq.com>
2018-09-15 21:46:02 +0800

    添加公共模块common

---
+++ common/src/main/java/com/github/baifenghe/common/constant/CommonConst.java
@@ -0,0 +1,23 @@
+package com.github.baifenghe.common.constant;
+
+/**
+ * 通用常量类
+ *
+ * @author bfh
+ * @since 1.0.0
+ */
+public final class CommonConst {
+
+    /**
+     * 成功
+     */
+    public final static int SUCCESS = 1;
+
+    /**
+     * 失败
+     */
+    public final static int FAILED = 0;
+
+
+
+}
