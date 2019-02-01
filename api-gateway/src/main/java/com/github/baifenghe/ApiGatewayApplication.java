76b7c418ec8142f1dc954cf6bcaaf18f2694434c
baifenghe <1170126481@qq.com>
2019-02-01 15:45:14 +0800

    :sparkles: spring-cloud-gateway swagger接口文档聚合

--- api-gateway/src/main/java/com/github/baifenghe/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/github/baifenghe/ApiGatewayApplication.java
@@ -1,13 +1,13 @@
 package com.github.baifenghe;
 
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author bfh
+ * @since 1.0.0
+ */
 @SpringBootApplication
-@EnableZuulProxy
-@EnableSwagger2Doc
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
---
+++ api-gateway/src/main/java/com/github/baifenghe/gateway/config/SwaggerProvider.java
@@ -0,0 +1,55 @@
+package com.github.baifenghe.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bfh
+ * @since 1.0.0
+ */
+@Component
+@Primary
+public class SwaggerProvider implements SwaggerResourcesProvider {
+
+    @Value("${spring.application.name}")
+    public String applicationName;
+
+    public static final String API_URI = "/v2/api-docs";
+
+    private final RouteLocator routeLocator;
+
+    public SwaggerProvider(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+
+        List<SwaggerResource> resources = new ArrayList<>();
+        routeLocator.getRoutes().subscribe(route -> {
+            String routeUri = route.getUri().getHost();
+            if (!applicationName.equalsIgnoreCase(routeUri)) {
+                resources.add(swaggerResource(routeUri, String.format("/%s/v2/api-docs", routeUri)));
+            }
+        });
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setSwaggerVersion("2.0");
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        return swaggerResource;
+    }
+
+
+}
